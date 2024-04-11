16bfa0a1450aa5c9b61d8b3052d797aed553fe6a
Siddharth Basumatary <74782752+miimmiicc@users.noreply.github.com>
2024-04-12 03:34:28 +0530

    Navbar and Google Authentication

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { getAuthSession } from "@/lib/nextauth";
+import React from "react";
+import Link, { LinkProps } from "next/link";
+import SignInButton from "./SigninButton";
+
+type Props = {};
+
+const Navbar = async(props: Props) => { 
+    return ( 
+        <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
+            <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
+                <Link href={"/"} className="flex items-center gap-2">
+                    <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+                        Kuizzy
+                    </p>
+                </Link>
+                <div className="flex items-center">
+                    <SignInButton text="Sign In"></SignInButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,4 +1,4 @@
-import { DefaultSession, NextAuthOptions } from "next-auth";
+import { DefaultSession, getServerSession, NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./db"
 import GoogleProvider from 'next-auth/providers/google'
@@ -52,3 +52,7 @@ export const authOptions: NextAuthOptions = {
     })
   ]
 }
+
+export const getAuthSession = () => { 
+  return getServerSession(authOptions);
+};
