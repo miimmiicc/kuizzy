9fe013b877f6c1473269eca30739c92728810f2a
Siddharth Basumatary <74782752+miimmiicc@users.noreply.github.com>
2024-04-13 15:26:21 +0530

    Quiz Dashboard and Schema updated

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -1,19 +1,120 @@
-import React from 'react'; 
+'use client';
+import React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { useForm } from 'react-hook-form'
+import { quizCreationSchema } from '@/schemas/form/quiz';
+import { input, z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Button } from './ui/button';
+import { Input } from "@/components/ui/input"
+import { BookOpen, CopyCheck } from 'lucide-react';
+import { Separator } from './ui/separator';
 
-type Props = {}; 
+type Props = {};
 
-const QuizCreation = (props: Props) => { 
-    return(
-        <div className="absolute top-1/2 left-1/2 trasnslate-x-1/2 -translate-y-1/2">
+type Input = z.infer<typeof quizCreationSchema>
+
+const QuizCreation = (props: Props) => {
+    const form = useForm<Input>({
+        resolver: zodResolver(quizCreationSchema),
+        defaultValues: {
+            amount: 3,
+            topic: "",
+            type: 'open_ended'
+        }
+    });
+
+    function onSubmit(input: Input) {
+        alert(JSON.stringify(input, null, 2))
+    }
+
+    form.watch();
+
+    return (
+        <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
             <Card>
                 <CardHeader>
                     <CardTitle className="text-2x font-bold">Quiz Creation</CardTitle>
-                        <CardDescription>Choose a topic
-                        </CardDescription>
+                    <CardDescription>Choose a topic
+                    </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="topic"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Topic</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter a Topic" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Please provide a topic
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="amount"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Topic</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Number of Questions"
+                                                {...field}
+                                                type="number"
+                                                min={1}
+                                                max={10}
+                                                onChange={(e) => {
+                                                    form.setValue('amount', parseInt(e.target.value))
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="flex justify-between">
+                                <Button
+                                    type='button'
+                                    onClick={() => {
+                                        form.setValue('type', 'mcq')
+                                    }}
+                                    className="w-1/2 rounded-none rounded-l-lg"
+                                    variant={form.getValues('type') === 'mcq' ? "default" : "secondary"}
+                                >
+                                    <CopyCheck className='h-4 w-4 mr-2' />Multiple Choice
+                                </Button>
+                                <Separator orientation="vertical" />
+                                <Button
+                                    type='button'
+                                    onClick={() => {
+                                        form.setValue('type', 'open_ended')
+                                    }}
+                                    className='w-1/2 rounded-none rounded-r-lg'
+                                    variant={form.getValues('type') === 'open_ended' ? "default" : "secondary"}
+                                >
+                                    <BookOpen className='w-4 h-4 mr-2' /> Open Ended
+                                </Button>
+                            </div>
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+
                 </CardContent>
             </Card>
         </div>
