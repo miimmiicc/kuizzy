9c2cadcd12345e80301aa3d2721ed722cad463bd
Siddharth Basumatary <74782752+miimmiicc@users.noreply.github.com>
2024-04-17 17:33:08 +0530

    answer counter and keyboard functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <Toaster />
         </Providers>
       </body>
     </html>
--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Game, Question } from "@prisma/client";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Card,
   CardDescription,
@@ -10,15 +10,16 @@ import {
 import { Button, buttonVariants } from "./ui/button";
 // import { differenceInSeconds } from "date-fns";
 import Link from "next/link";
-import { BarChart, ChevronRight, Loader2, Timer } from "lucide-react";
+import { BarChart, ChevronRight, Currency, Loader2, Timer } from "lucide-react";
 // import { checkAnswerSchema, endGameSchema } from "@/schemas/questions";
 // import { cn, formatTimeDelta } from "@/lib/utils";
-// import MCQCounter from "./MCQCounter";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { z } from "zod";
 import MCQCounter from "./MCQCounter";
-// import { useToast } from "./ui/use-toast";
+import { checkAnswerSchema } from "@/schemas/form/quiz";
+import { toast, useToast } from "./ui/use-toast";
+import { cn } from "@/lib/utils";
 
 type Props = {
   game: Game & { questions: Pick<Question, "id" | "options" | "question">[] };
@@ -33,10 +34,71 @@ const MCQ = ({ game }: Props) => {
   //   });
   const [selectedChoice, setSelectedChoice] = React.useState<number>(0);
   //   const [now, setNow] = React.useState(new Date());
-
+  const [correctAnswers, setCorrectAnswers] = React.useState<number>(0);
+  const [wrongAnswers, setWrongAnswers] = React.useState<number>(0);
+  const [hasEnded, setHasEnded] = React.useState<boolean>(false);
   const currentQuestion = React.useMemo(() => {
     return game.questions[questionIndex];
   }, [questionIndex, game.questions]);
+
+  const { mutate: checkAnswer, isPending: isChecking } = useMutation({
+    mutationFn: async () => {
+      const payload: z.infer<typeof checkAnswerSchema> = {
+        questionId: currentQuestion.id,
+        userAnswer: options[selectedChoice]
+      }
+      const response = await axios.post('/api/checkAnswer', payload);
+      return response.data
+    }
+  });
+
+  const handleNext = React.useCallback(() => {
+    if (isChecking) return;
+    checkAnswer(undefined, {
+      onSuccess: ({ isCorrect }) => {
+        if (isCorrect) {
+          toast({
+            title: "Correct!",
+            description: "Correct Answer",
+            variant: 'success'
+          })
+          setCorrectAnswers((prev) => prev + 1)
+        } else {
+          toast({
+            title: "Incorrect!",
+            description: "Incorrect Answer",
+            variant: 'destructive'
+          })
+          setWrongAnswers((prev) => prev + 1);
+        }
+        if (questionIndex === game.questions.length - 1) {
+          setHasEnded(true);
+          return;
+        }
+        setQuestionIndex((prev) => prev + 1);
+      },
+    });
+  }, [checkAnswer, toast, isChecking, questionIndex, game.questions.length]);
+
+  React.useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key == '1') {
+        setSelectedChoice(0);
+      } else if (event.key === '2') {
+        setSelectedChoice(1);
+      } else if (event.key === '3') {
+        setSelectedChoice(2);
+      } else if (event.key === '4') {
+        setSelectedChoice(3);
+      } else if (event.key === 'Enter') {
+        handleNext();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNext]);
 
   const options = React.useMemo(() => {
     if (!currentQuestion) return [];
@@ -44,6 +106,21 @@ const MCQ = ({ game }: Props) => {
     return JSON.parse(currentQuestion.options as string) as string[];
   }, [currentQuestion]);
 
+  if (hasEnded) {
+    return (
+      <div className="absolute flex flex-col justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="px-4 mt-2 font-semibold text-white bg-green-500 rounded-md whitespace-nowrap">
+          You completed in {'3m 4s'}
+        </div>
+        <Link
+          href={`/statistics/${game.id}`}
+          className={cn(buttonVariants(), "mt=2")}>
+          View Statistics
+          <BarChart className="w-4 h-4 ml-2" />
+        </Link>
+      </div>
+    );
+  }
   //   const { toast } = useToast();
   //   const { mutate: checkAnswer, isLoading: isChecking } = useMutation({
   //     mutationFn: async () => {
@@ -169,8 +246,8 @@ const MCQ = ({ game }: Props) => {
           </div>
         </div>
         <MCQCounter
-          correct_answers={3}
-          wrong_answers={4}
+          correctAnswers={correctAnswers}
+          wrongAnswers={wrongAnswers}
         />
       </div>
       <Card className="w-full mt-4">
@@ -208,13 +285,13 @@ const MCQ = ({ game }: Props) => {
         <Button
           //   variant="default"
           className="mt-2"
-        //   size="lg"
-        //   disabled={isChecking || hasEnded}
-        //   onClick={() => {
-        //     handleNext();
-        //   }}
+          //   size="lg"
+          disabled={isChecking}
+          onClick={() => {
+            handleNext();
+          }}
         >
-          {/* {isChecking && <Loader2 className="w-4 h-4 mr-2 animate-spin" />} */}
+          {isChecking && <Loader2 className="w-4 h-4 mr-2 animate-spin" />}
           Next <ChevronRight className="w-4 h-4 ml-2" />
         </Button>
       </div>
